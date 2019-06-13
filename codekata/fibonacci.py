08c90414fe75926de2fc81a3013d79d29378fe6b
Manikandan99 <51670133+Manikandan99@users.noreply.github.com>
2019-06-13 11:49:05 +0530

    Update fibonacci.py

--- codekata/fibonacci.py
+++ codekata/fibonacci.py
@@ -1,5 +1,10 @@
 f=[1,1]
-m=int(input())
-for i in range(2,m):
-  f.append(f[i-1]+f[i-2])
-print(*f,sep=' ')
+a=int(input())
+if(a==0):
+  print("0")
+elif(a==1):
+  print("1")
+else:
+  for i in range(2,a):
+    f.append(f[i-1]+f[i-2])
+  print(*f,sep=' ')
