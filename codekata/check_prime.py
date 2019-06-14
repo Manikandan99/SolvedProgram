ec86b8d0f44ab58a771bf65c144fcaf011d9e784
Manikandan99 <51670133+Manikandan99@users.noreply.github.com>
2019-06-14 09:39:18 +0530

    Update check_prime.py

--- codekata/check_prime.py
+++ codekata/check_prime.py
@@ -1,15 +1,15 @@
-a=int(input())
-m=0
-if(a==2 or a==3):
+m1=int(input())
+k=0
+if(m1==2 or m1==3):
   print("yes")
 else:
-  for i in range(2,a):
-    if(a%i==0):
-      m=0
+  for i in range(2,m1):
+    if(m1%i==0):
+      k=0
       print("no")
       break
     else:
-     m=1
+     k=1
      continue  
-  if(m!=0):
+  if(k!=0):
     print("yes")
