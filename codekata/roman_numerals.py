e18bc34d0a828bb097f7169a81c4ab2a22c25c24
Manikandan99 <51670133+Manikandan99@users.noreply.github.com>
2019-06-17 22:07:41 +0530

    Update roman_numerals.py

--- codekata/roman_numerals.py
+++ codekata/roman_numerals.py
@@ -1,5 +1,5 @@
-lm1=int(input())
+lm1=input()
 lm2=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
 for i in range(21):
-  if(lm1==i):
-    print(lm2[i])
+  if(lm1==lm2[i]):
+    print(i)
