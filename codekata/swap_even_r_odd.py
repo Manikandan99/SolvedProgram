0b4a674239c1f94a61907e3bf8b7f7ab2dd27041
Manikandan99 <51670133+Manikandan99@users.noreply.github.com>
2019-07-28 18:32:09 +0530

    Update swap_even_r_odd.py

--- codekata/swap_even_r_odd.py
+++ codekata/swap_even_r_odd.py
@@ -1,9 +1,9 @@
-a=input()
-l=[]
+m1=input()
+m2=[]
 t=0
-for i in a:
-  l.append(i)
-for i in range(0,len(a)):
+for i in m1:
+  m2.append(i)
+for i in range(0,len(m1)):
   if(i%2!=0):
-    l[i-1],l[i]=l[i],l[i-1]
-print(*l,sep="")
+    m2[i-1],m2[i]=m2[i],m2[i-1]
+print(*m2,sep="")
