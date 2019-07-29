1c73c226778e277fa3aca98102710f80485b882c
Manikandan99 <51670133+Manikandan99@users.noreply.github.com>
2019-07-29 21:06:56 +0530

    Update LCM.py

--- codekata/LCM.py
+++ codekata/LCM.py
@@ -1,5 +1,5 @@
 from math import gcd
-mk1,mk2 = map(int,input().split())
-mk3=gcd(mk1,mk2)
-lcm=(mk1*mk2) // mk3
-print(lcm)
+mani1,mani2 = map(int,input().split())
+mk3=gcd(mani1,mani2)
+harsha=(mani1*mani2) // mk3
+print(harsha)
