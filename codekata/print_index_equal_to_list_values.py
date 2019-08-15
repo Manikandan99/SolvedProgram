43eb2eba9202edd70f459f525ffc1e88466fde88
Manikandan99 <51670133+Manikandan99@users.noreply.github.com>
2019-08-15 14:54:11 +0530

    Update print_index_equal_to_list_values.py

--- codekata/print_index_equal_to_list_values.py
+++ codekata/print_index_equal_to_list_values.py
@@ -1,5 +1,10 @@
 n=int(input())
+m=[]
 mk=list(map(int,input().split()))
 for i in range(0,len(mk)):
   if(mk[i]==i):
-    print(mk[i],end=" ")
+    m.append(mk[i])
+if(len(m)!=0):
+  print(*m,sep=" ")
+else:
+  print("-1")
