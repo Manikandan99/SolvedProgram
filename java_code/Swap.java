ab5870be26a7c8a6af637befddec30ff18bbb048
Manikandan99 <51670133+Manikandan99@users.noreply.github.com>
2019-09-14 21:58:13 +0530

    Create Swap.java

---
+++ java_code/Swap.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class swapping {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+		System.out.println("Before swap: "+a+" "+b);
+		String t="";
+		t=a;
+		a=b;
+		b=t;
+		System.out.print("After swap: "+a+" "+b);	
+	}
+}
+
+
+10 20
+Before swap: 10 20
+After swap: 20 10
