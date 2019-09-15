8fc8bbd218dcd4bd83cc2a59b9d1a7bbc95db3ce
Manikandan99 <51670133+Manikandan99@users.noreply.github.com>
2019-09-15 11:24:46 +0530

    Create String_to_character_array.java

---
+++ java_code/String_to_character_array.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class stringfun {
+	public static void main(String args[]){
+		Scanner sc = new Scanner (System.in);
+		String x = sc.next();
+		char []y=x.toCharArray();
+		for(char i:y){
+		System.out.println(i);
+		}	
+	}
+}
