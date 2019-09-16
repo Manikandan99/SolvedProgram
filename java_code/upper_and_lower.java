523c41e93185a696c01920268af1cea30f5c4ece
Manikandan99 <51670133+Manikandan99@users.noreply.github.com>
2019-09-16 07:26:19 +0530

    Create upper_and_lower.java

---
+++ java_code/upper_and_lower.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class stringrev {
+	public static void main(String[] args){
+	Scanner sc = new Scanner (System.in);
+	String x = sc.next();
+	String y ="",z="";
+	y=x.toLowerCase();
+	z=x.toUpperCase();
+	System.out.print(y+"\n"+z);	
+   }
+}
